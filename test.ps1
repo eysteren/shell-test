24d51427737ef78052d7a69f0d223bbbf3754dfb
eysteren <ekrem@cloudpeer.com>
2022-08-12 15:20:35 +0300

    Ä°lk Commit

--- test.ps1
+++ test.ps1
@@ -1,1 +1,21 @@
-Write-Host "test-branch1"
+Write-Host "Run html"
+
+$Path = "C:\Users\Ekrem\Desktop\yuklenecekler"
+$Divider = "\"
+$CommitMsg = "İlk Commit"
+
+git config --global user.email "ekrem@cloudpeer.com"
+git config --global user.name "eysteren"
+
+Write-Host $Path $Divider
+
+git add .
+git commit -m $CommitMsg
+git push
+
+
+
+#Start-Process "C:\Program Files\Google\Chrome\Application\chrome.exe" "C:\Users\Ekrem\Desktop\powershell-ornek\shell-test\deneme.html"
+
+
+#Get-Content C:\Users\Ekrem\Desktop\powershell-ornek\shell-test\deneme.html
